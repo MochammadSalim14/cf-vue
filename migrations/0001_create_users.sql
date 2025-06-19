58c11cc5561e39b09da463fefe43f99733db0b79
MochammadSalim14 <salimunsur14@gmail.com>
2025-06-19 20:23:30 +0700

    all

---
+++ migrations/0001_create_users.sql
@@ -0,0 +1,9 @@
+-- Migration number: 0001 	 2025-06-19T13:08:38.683Z
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    address TEXT
+);
+
+INSERT INTO users(name, address) VALUES ('salim', 'pasuruan'), ('lukman', 'Sukorejo'), ('Ana', 'surabaya');
